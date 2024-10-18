e2df73a0589b225ed0cae6f0c61967697dfb207d
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-10-18 19:06:22 +0300

    Am adaugat JpaRepository

--- src/main/java/com/arturfrimu/materialecalculator/MaterialRepository.java
+++ src/main/java/com/arturfrimu/materialecalculator/MaterialRepository.java
@@ -1,25 +1,9 @@
 package com.arturfrimu.materialecalculator;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @Repository
-public class MaterialRepository {
-    private static final Map<Long, MaterialsEntity> map = new HashMap<>();
-
-    public void addMaterials(MaterialsEntity material) {
-        map.put(material.getId(), material);
-    }
-
-    public Optional<MaterialsEntity> findById(Long id) {
-        return Optional.ofNullable(map.get(id));
-    }
-
-    public List<MaterialsEntity> findAll() {
-        return map.values().stream().toList();
-    }
+public interface MaterialRepository extends JpaRepository<MaterialsEntity, Long> {
+    // Poți adăuga metode personalizate aici, dacă este necesar
 }
